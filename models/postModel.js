2de1c6a7f6d22036f129cfbffac1ceaf61a7e272
Shubham Karkhile <shubhz6599@gmail.com>
2024-09-10 16:00:20 +0530

    changes

--- models/postModel.js
+++ models/postModel.js
@@ -4,7 +4,10 @@ const postSchema = new mongoose.Schema({
     title: String,
     content: String,
     category: String,
-    image: String,  // Field for image URL
+    image: {
+        data: Buffer, // Store the image as binary data
+        contentType: String // Store the content type (e.g., 'image/png')
+    },
     username: String,
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Users' }],
